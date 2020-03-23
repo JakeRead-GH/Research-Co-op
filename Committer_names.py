03642a6d60bca4081bf03be383644f4cf53d7aa4
Jake Read <61603048+JakeRead-GH@users.noreply.github.com>
2020-03-23 13:57:22 -0400

    Add files via upload

--- Committer_names.py
+++ Committer_names.py
@@ -1,16 +1,44 @@
 file = open("log.txt")
+names = []
+commits = []
+blank_lines = 0
+authors = 0
 still_reading = True
-print("hello")
 
-#while still_reading:
-for a in range(10):
-    line = file.readline()
-    print(line)
+while still_reading:
+    line = file.readline().strip()
 
-    if line.find("Author:") != -1:
-        print("y")
-        still_reading = False
+    if (line.find("Author:") != -1):
+        before_keyword, keyword, name = line.partition("Author:")
+        name = name.lstrip()
+        if name not in names:
+            names.append(name)
+            commits.append(int(1) )
+            authors = authors + 1
+
+        else:
+            position = names.index(name)
+            commits[position] = commits[position] + 1
+
+    elif line == "":
+        blank_lines = blank_lines + 1
+        
+        if blank_lines == 3:
+            still_reading = False
+
     else:
-        print("n")
+        blank_lines = 0
+
+for a in range(authors):
+    print("Author: " + names[a])
+    print("Commits: " + str(commits[a]) )
+    print("")
 
 file.close()
+
+
+
+## Code for removing email if required
+
+#name = ' '.join(after_keyword.split()[:2])
+#        print(name)
